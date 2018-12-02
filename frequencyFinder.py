ccdb2700aee915d85926fd212d9a360c70448885
jinglebiscuits <scott.wehby@gmail.com>
2018-12-01 18:15:52 -0600

    day1

--- frequencyFinder.py
+++ frequencyFinder.py
@@ -1,6 +1,15 @@
 frequency = 0
-file = open('input.txt', 'r')
-for line in file:
-    frequency += int(line)
-file.close()
+frequencyDictionary = {0:0}
+lookingForDupe = True
+while lookingForDupe:
+    file = open('input.txt', 'r')
+    for line in file:
+        frequency += int(line)
+        if (frequency not in frequencyDictionary):
+            frequencyDictionary[frequency] = frequency
+        else:
+            print "first dupe is {}".format(frequency)
+            lookingForDupe = False
+            break
+    file.close()
 print str(frequency)
